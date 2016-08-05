35048152bf18283da7349c798dd9240403d57ed2
Peter Hebert <peter@peterhebert.com>
2016-08-04 19:00:11 -0700

    first commit
    readme, license, package.json, Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+grunt.initConfig({
+  sass: {
+    dist: {
+			options: {
+				style: 'compressed'
+			},
+      files: [{
+        expand: true,
+        cwd: 'scss',
+        src: ['*.scss'],
+        dest: 'css',
+        ext: '.css'
+      }]
+    }
+  },
+  watch: {
+    css: {
+      files: 'scss/**/*.scss',
+      tasks: ['sass'],
+    },
+  },
+});
+
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+grunt.registerTask('default', ['watch']);
+
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Rex Rana presentation slides
+
+Presentation templates for the [reveal.js](http://lab.hakim.se/reveal-js/) framework.
+
+## Templates
+- [Dark](dark.html)
+- [Light](light.html)
+
+## License
+
+[GNU GENERAL PUBLIC LICENSE Version 3](LICENSE)
+
+Copyright (C) 2016 Rex Rana Design and Development Ltd., https://rexrana.ca
