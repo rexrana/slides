cfd9ce8ee198c6469da8339bba406c17289e9dc2
Peter Hebert <peter@peterhebert.com>
2016-08-25 10:43:26 -0700

    removed chrome detecttion and auto print of PDF

--- js/print-pdf.js
+++ js/print-pdf.js
@@ -3,9 +3,3 @@ link.rel = 'stylesheet';
 link.type = 'text/css';
 link.href = window.location.search.match( /print-pdf/gi ) ? '../reveal.js/css/print/pdf.css' : '../reveal.js/css/print/paper.css';
 document.getElementsByTagName( 'head' )[0].appendChild( link );
-
-var isPDF = window.location.search.match( /print-pdf/gi );
-var isChrome = !!window.chrome && !!window.chrome.webstore;
-if(isPDF && isChrome) {
-  window.print();
-}
