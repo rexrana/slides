759e5c3e7ed92381e6366ea6fe8d9c8b87029408
Peter Hebert <peter@peterhebert.com>
2018-02-21 15:05:49 -0800

    fixed references for reveal js plugins in init script

--- js/reveal-init.js
+++ js/reveal-init.js
@@ -8,9 +8,9 @@ Reveal.initialize({
 
   // More info https://github.com/hakimel/reveal.js#dependencies
   dependencies: [
-    { src: '../reveal.js/plugin/markdown/marked.js' },
-    { src: '../reveal.js/plugin/markdown/markdown.js' },
-    { src: '../reveal.js/plugin/notes/notes.js', async: true },
-    { src: '../reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+    { src: '../node_modules/reveal.js/plugin/markdown/marked.js' },
+    { src: '../node_modules/reveal.js/plugin/markdown/markdown.js' },
+    { src: '../node_modules/reveal.js/plugin/notes/notes.js', async: true },
+    { src: '../node_modules/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
   ]
 });
